e8350838bab7c57bd1640a81288502f8e12bc076
BB-choi <choibobae0809@gmail.com>
2023-02-20 12:00:59 +0900

    refactor: theme.js 파일 분리

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,20 @@ import GlobalStyle from "./GlobalStyle";
 import Basket from "./pages/Basket";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
+import theme from "./theme";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider
-        theme={{
-          color: {
-            black: "#000",
-            white: "#FFF",
-            primary01: "#854BFF",
-            primary02: "#D7CBEF",
-            secondary01: "#24DBAF",
-            secondary02: "#CDF3EA",
-            gray01: "#212121",
-            gray02: "#616161",
-            gray03: "#A7A7A7",
-            gray04: "#ccc",
-            gray05: "#eee",
-            gray06: "#F5F5F5",
-          },
-        }}
-      >
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
         <GlobalStyle />
         <Routes>
           <Route index element={<Home />} />
           <Route path="product/:productId" element={<ProductDetail />} />
           <Route path="basket" element={<Basket />} />
         </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
